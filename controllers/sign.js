ea28bc826d34f986f50b9486c9314d63660bf7c5
mingjiu <hmj0718@163.com>
2019-08-20 17:07:38 +0800

    取消注册邮箱激活

--- controllers/sign.js
+++ controllers/sign.js
@@ -67,7 +67,7 @@ exports.signup = function (req, res, next) {
           return next(err);
         }
         // 发送激活邮件
-        mail.sendActiveMail(email, utility.md5(email + passhash + config.session_secret), loginname);
+        // mail.sendActiveMail(email, utility.md5(email + passhash + config.session_secret), loginname);
         res.render('sign/signup', {
           success: '欢迎加入 ' + config.name + '！我们已给您的注册邮箱发送了一封邮件，请点击里面的链接来激活您的帐号。'
         });
