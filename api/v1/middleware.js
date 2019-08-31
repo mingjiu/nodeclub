bc662395a26c9e76840968e63268c3bd76f94035
mingjiu <hmj0718@163.com>
2019-08-31 14:40:24 +0800

    api 使用jwt验证登录状态

--- api/v1/middleware.js
+++ api/v1/middleware.js
@@ -1,28 +1,58 @@
 var UserModel  = require('../../models').User;
 var eventproxy = require('eventproxy');
 var validator  = require('validator');
+var jwt = require('jsonwebtoken')
+var cache        = require('../../common/cache')
 
 // 非登录用户直接屏蔽
 var auth = function (req, res, next) {
   var ep = new eventproxy();
   ep.fail(next);
 
-  var accessToken = String(req.body.accesstoken || req.query.accesstoken || '');
-  accessToken = validator.trim(accessToken);
+  var accessToken = req.headers.authorization.replace(/Bearer[ ]*/g, '')
+  
+  if (!accessToken) {
+    res.status(401)
+    return res.send({success: false, error_msg: '验证过期'})
+  } else {
+    var sessionKey = jwt.verify(accessToken, 'ZHGJ-LITE-holyshit')
+    sessionKey = sessionKey.sessionKey
+    cache.get(`sessionKey:${sessionKey}`, ep.done(function (data) {
 
-  UserModel.findOne({accessToken: accessToken}, ep.done(function (user) {
-    if (!user) {
-      res.status(401);
-      return res.send({success: false, error_msg: '错误的accessToken'});
-    }
-    if (user.is_block) {
-      res.status(403);
-      return res.send({success: false, error_msg: '您的账户被禁用'});
-    }
-    req.user = user;
-    next();
-  }));
+      if(!data.active){
+        res.send({
+          code: 9000,
+          success: false,
+          message: 'user is not exist'
+        })
+      }else {
+        UserModel.findOne({_id: data._id}, ep.done(function(user) {
+          req.user = user
+          next()
+        }))
+      }
+    }))
+    
+  }
+  
+  // var accessToken = String(req.body.accesstoken || req.query.accesstoken || '');
+  // accessToken = validator.trim(accessToken);
 
+  // UserModel.findOne({accessToken: accessToken}, ep.done(function (user) {
+  //   if (!user) {
+  //     res.status(401);
+  //     return res.send({success: false, error_msg: '错误的accessToken'});
+  //   }
+  //   if (user.is_block) {
+  //     res.status(403);
+  //     return res.send({success: false, error_msg: '您的账户被禁用'});
+  //   }
+  //   req.user = user;
+  //   next();
+  // }));
+
+
+  
 };
 
 exports.auth = auth;
@@ -32,20 +62,34 @@ var tryAuth = function (req, res, next) {
   var ep = new eventproxy();
   ep.fail(next);
 
-  var accessToken = String(req.body.accesstoken || req.query.accesstoken || '');
-  accessToken = validator.trim(accessToken);
+  var sessionKey = req.headers.sessionkey
+  if (!sessionKey) {
+    return next()
+  } else {
+    cache.get(`sessionKey:${sessionKey}`, ep.done(function(data) {
+      UserModel.findOne({_id: data._id}, ep.done(function(user){
+        console.log(user)
+        req.user = user
+        next()
+      }))
+    }))
+  }
+  
+  
+  // var accessToken = String(req.body.accesstoken || req.query.accesstoken || '');
+  // accessToken = validator.trim(accessToken);
 
-  UserModel.findOne({accessToken: accessToken}, ep.done(function (user) {
-    if (!user) {
-      return next()
-    }
-    if (user.is_block) {
-      res.status(403);
-      return res.send({success: false, error_msg: '您的账户被禁用'});
-    }
-    req.user = user;
-    next();
-  }));
+  // UserModel.findOne({accessToken: accessToken}, ep.done(function (user) {
+  //   if (!user) {
+  //     return next()
+  //   }
+  //   if (user.is_block) {
+  //     res.status(403);
+  //     return res.send({success: false, error_msg: '您的账户被禁用'});
+  //   }
+  //   req.user = user;
+  //   next();
+  // }));
 
 };
 
